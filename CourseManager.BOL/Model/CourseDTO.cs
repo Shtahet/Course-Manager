caa0351469f6fd56b5701dc81a6451eda065fa52
Alexandr Gergalo <derien001@gmal.com>
2018-03-14 01:34:20 +0200

    Создали модель-клас курса для бизнес уровня

---
+++ CourseManager.BOL/Model/CourseDTO.cs
@@ -0,0 +1,49 @@
+﻿using CourseManager.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManager.BOL.Model
+{
+    class CourseDTO
+    {
+        public int CoursID { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(4000)]
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public static explicit operator CourseDTO(Course obj)
+        {
+            if (obj == null) return null;
+
+            return new CourseDTO()
+            {
+                CoursID = obj.CoursID,
+                Name = obj.Name,
+                Description = obj.Description,
+                Price = obj.Price
+            };
+        }
+
+        public static explicit operator Course (CourseDTO obj)
+        {
+            if (obj == null) return null;
+
+            return new Course()
+            {
+                CoursID = obj.CoursID,
+                Name = obj.Name,
+                Description = obj.Description,
+                Price = obj.Price
+            };
+        }
+    }
+}
