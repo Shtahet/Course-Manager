0b37a09eaf7a5f1ea3cda4f883fd6c0f5fe334f8
Alexandr Gergalo <derien001@gmal.com>
2018-03-13 13:49:50 +0200

    Создали общий интерфейс для CRUD операций над объектами хранилища

---
+++ CourseManager.DAL/Abstract/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursManager.DAL.Abstract
+{
+    interface IGenericRepository<T> where T: class, new()
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> FindBy(Expression<Func<T, bool>> predicate);
+        T Get(int key);
+        void AddOrUpdate(T obj);
+        void Delete(T obj);
+    }
+}
