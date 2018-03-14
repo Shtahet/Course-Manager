fd623489ddc27fa0c923b3ea1fd951e095c987e8
Alexandr Gergalo <derien001@gmal.com>
2018-03-14 02:14:22 +0200

    Создали модель-клас расписания для бизнес уровня

---
+++ CourseManager.BOL/Model/ScheduleDTO.cs
@@ -0,0 +1,49 @@
+﻿using CourseManager.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManager.BOL.Model
+{
+    class ScheduleDTO
+    {
+        public int ScheduleID { get; set; }
+
+        public int CoursID { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+        public CourseDTO Course { get; set; }
+
+        public static explicit operator ScheduleDTO(Schedule obj)
+        {
+            if (obj == null) return null;
+
+            return new ScheduleDTO()
+            {
+                ScheduleID = obj.ScheduleID,
+                CoursID = obj.CoursID,
+                StartDate = obj.StartDate,
+                EndDate = obj.EndDate,
+                Course = (CourseDTO)obj.Course
+            };
+
+        }
+
+        public static explicit operator Schedule (ScheduleDTO obj)
+        {
+            if (obj == null) return null;
+
+            return new Schedule()
+            {
+                ScheduleID = obj.ScheduleID,
+                CoursID = obj.CoursID,
+                StartDate = obj.StartDate,
+                EndDate = obj.EndDate
+            };
+        }
+    }
+}
