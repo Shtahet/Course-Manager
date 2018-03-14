298eb66ac808d87e81b43cbfe6870fcff442bc41
Alexandr Gergalo <derien001@gmal.com>
2018-03-14 21:41:24 +0200

    Создали класс для конвертации Linq выражений из бизнесс уровня на уровень подлючения

---
+++ CourseManager.BOL/Concrete/ConvertTypeExpression.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManager.BOL.Concrete
+{
+    static class ConvertTypeExpression
+    {
+        public static Expression<Func<OutT, bool>> ConvertType<inT, OutT>(Expression expression)
+           where OutT : class
+        {
+
+            var param = Expression.Parameter(typeof(OutT), "x");
+
+            var result = new CustomExpVisitor<OutT>(param).Visit(expression);
+
+            Expression<Func<OutT, bool>> lambda = Expression.Lambda<Func<OutT, bool>>(result, new[] { param });
+
+            return lambda;
+        }
+        private class CustomExpVisitor<T> : ExpressionVisitor
+        {
+            ParameterExpression _param;
+
+            public CustomExpVisitor(ParameterExpression param)
+            {
+                _param = param;
+            }
+
+            protected override Expression VisitParameter(ParameterExpression node)
+            {
+                return _param;
+            }
+
+            protected override Expression VisitMember(MemberExpression node)
+            {
+                if (node.Member.MemberType == MemberTypes.Property)
+                {
+                    MemberExpression memberExpression = null;
+
+                    var memberName = node.Member.Name;
+                    var otherMember = typeof(T).GetProperty(memberName);
+
+                    memberExpression = Expression.Property(Visit(node.Expression), otherMember);
+
+                    return memberExpression;
+                }
+                else
+                {
+                    return base.VisitMember(node);
+                }
+            }
+        }
+    }
+}
