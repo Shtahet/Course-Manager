29206a09af1ead522b7eb6ba635db504efb040a4
Alexandr Gergalo <derien001@gmal.com>
2018-03-14 22:04:35 +0200

    Создали реализацию сервия управления расписанием

---
+++ CourseManager.BOL/Concrete/ScheduleDTOService.cs
@@ -0,0 +1,47 @@
+﻿using CourseManager.BOL.Model;
+using CourseManager.DAL.Abstract;
+using CourseManager.DAL.Model;
+using Sellers.BOL.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManager.BOL.Concrete
+{
+    class ScheduleDTOService : IBolService<ScheduleDTO>
+    {
+        IGenericRepository<Schedule> scheduleRepo;
+        public ScheduleDTOService(IGenericRepository<Schedule> DISchedule)
+        {
+            scheduleRepo = DISchedule;
+        }
+        public void AddOrUpdate(ScheduleDTO obj)
+        {
+            scheduleRepo.AddOrUpdate((Schedule)obj);
+        }
+
+        public void Delete(ScheduleDTO obj)
+        {
+            scheduleRepo.Delete((Schedule)obj);
+        }
+
+        public IEnumerable<ScheduleDTO> FindBy(Expression<Func<ScheduleDTO, bool>> predicate)
+        {
+            Expression<Func<Schedule, bool>> expressionDAL = ConvertTypeExpression.ConvertType<ScheduleDTO, Schedule>(predicate.Body);
+            return scheduleRepo.FindBy(expressionDAL).ToList().Select(s => (ScheduleDTO)s);
+        }
+
+        public ScheduleDTO Get(int id)
+        {
+            return (ScheduleDTO)scheduleRepo.Get(id);
+        }
+
+        public IEnumerable<ScheduleDTO> GetAll()
+        {
+            return scheduleRepo.GetAll().ToList().Select(s => (ScheduleDTO)s);
+        }
+    }
+}
