ec7e659899260eb5a1f2c538123662cb29d18f93
Alexandr Gergalo <derien001@gmal.com>
2018-03-15 02:16:28 +0200

    Создали класс конфигурации зависимостей для Autofac

---
+++ CourseManager.WebUI/Infrastructure/AutofacConfigModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using System.Data.Entity;
+using CourseManager.DAL.Model;
+using CourseManager.DAL.Abstract;
+using CourseManager.DAL.Concrete;
+using CourseManager.BOL.Model;
+using Sellers.BOL.Abstract;
+using CourseManager.BOL.Concrete;
+
+namespace CourseManager.WebUI.Infrastructure
+{
+    public class AutofacConfigModule: Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType(typeof(CourseContext)).As(typeof(DbContext)).InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(GenericRepository<>)).As(typeof(IGenericRepository<>)).InstancePerRequest();
+            builder.RegisterType(typeof(CourseDTOService)).As(typeof(IBolService<CourseDTO>)).InstancePerRequest();
+            builder.RegisterType(typeof(CourseDTOService)).As(typeof(IBolService<ScheduleDTO>)).InstancePerRequest();
+            base.Load(builder);
+        }
+    }
+}
