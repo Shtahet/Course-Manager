186fda38ddd2880b964227393a6cad861611b013
Alexandr Gergalo <derien001@gmal.com>
2018-03-17 18:24:22 +0200

    Создали контроллер для управления расписаниями. Метод для выбора списка расписаний и частичное представление к нему

---
+++ CourseManager.WebUI/Controllers/ScheduleController.cs
@@ -0,0 +1,28 @@
+﻿using CourseManager.BOL.Model;
+using Sellers.BOL.Abstract;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace CourseManager.WebUI.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        IBolService<ScheduleDTO> scheduleService;
+        
+        public ScheduleController(IBolService<ScheduleDTO> DIScheduleService)
+        {
+            scheduleService = DIScheduleService;
+        }
+        // GET: Schedule
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ScheduleList (int courseID)
+        {
+            IEnumerable<ScheduleDTO> model = scheduleService.FindBy(s => s.CoursID == courseID);
+            return PartialView(model);
+        }
+    }
+}
