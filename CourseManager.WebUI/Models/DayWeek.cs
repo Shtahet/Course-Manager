4394c8ce8c64977c95724c41d69d16854071f004
Alexandr Gergalo <derien001@gmal.com>
2018-03-18 00:50:27 +0200

    Создали класс-модель для создания списка дней недели

---
+++ CourseManager.WebUI/Models/DayWeek.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace CourseManager.WebUI.Models
+{
+    public class DayWeek
+    {
+        private DayOfWeek day;
+        public int DayNumber
+        {
+            get { return (int)day; }
+            set { day = (DayOfWeek)value; }
+        }
+        public string DayName
+        {
+            get { return Thread.CurrentThread.CurrentCulture.DateTimeFormat.GetDayName(day); }
+            set { day = (DayOfWeek)Array.IndexOf(Thread.CurrentThread.CurrentCulture.DateTimeFormat.DayNames, value); }
+        }
+    }
+}
