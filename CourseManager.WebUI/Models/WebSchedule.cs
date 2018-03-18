8d06d5183665b19262515bf085dbcb5ff4fbab2a
Alexandr Gergalo <derien001@gmal.com>
2018-03-19 00:12:47 +0200

    Исправили set методы для присваивания значений дате

--- CourseManager.WebUI/Models/WebSchedule.cs
+++ CourseManager.WebUI/Models/WebSchedule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -14,40 +15,43 @@ namespace CourseManager.WebUI.Models
         public int StartHour
         {
             get { return startDate.Hour; }
-            set { startDate.AddHours(value); }
+            set { startDate = startDate.AddHours(value); }
         }
         public int StartMinute
         {
             get { return startDate.Minute; }
-            set { startDate.AddMinutes(value); }
+            set { startDate = startDate.AddMinutes(value); }
         }
         public int EndHour
         {
             get { return endDate.Hour; }
-            set { endDate.AddHours(value); }
+            set { endDate = endDate.AddHours(value); }
         }
         public int EndMinute
         {
             get { return endDate.Minute; }
-            set { endDate.AddMinutes(value); }
+            set { endDate = endDate.AddMinutes(value); }
         }
+        [Display(Name = "Время начала")]
         public DateTime StartDate
         {
             get { return startDate; }
             set { startDate = value; }
         }
+        [Display(Name = "Время окончания")]
         public DateTime EndDate
         {
             get { return endDate; }
             set { endDate = value; }
         }
+        [Display(Name = "День занятий")]
         public int DayOfWeek
         {
             get { return (int)startDate.DayOfWeek; }
             set
             {
-                startDate.AddDays(value);
-                endDate.AddDays(value);
+                startDate = startDate.AddDays(value);
+                endDate = endDate.AddDays(value);
             }
         }
     }
